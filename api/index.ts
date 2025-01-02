468bf60caad1641db1dfe941c7722408209aebef
きむしゅん <96727208+kimshun0213kr@users.noreply.github.com>
2025-01-02 00:43:22 +0000

    Update: express.json()を追加

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import userController from "./userController/userController";
 const app = express();
 
+app.use(express.json());
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/user", userController);
 
